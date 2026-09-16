817e63ae1d5aa294ea1254e4c8ecb5c8729d30a3
yousefghanem501-gif <yousefghanem501@gmail.com>
2026-09-15 22:32:34 -0700

    Remove network image URL from Constant class
    Removed hardcoded network image URL from Constant class.

--- project_1/lib/constant.dart
+++ project_1/lib/constant.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 class Constant {
-  static String networkImage="https://imgs.search.brave.com/Dfzi8iBBUk1OQX7WfX-dkm-Qklbenq3jKHdivoIotDw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8z/LzNkL0xpb25lbF9N/ZXNzaV9ORV9SZXZv/bHV0aW9uX0ludGVy/X01pYW1pXzcuOS4y/NS0wNTUuanBn";
   static Color primary=Color(0xff15181e);
 
 }
